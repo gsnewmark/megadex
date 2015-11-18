0522dcfe3d07aaaf7ab241ca28595301af15c686
Ivan Kryvoruchko <gildraug@gmail.com>
2015-11-18 23:36:35 +0200

    Finish union tutorial

--- src/cljs/megadex/app.cljs
+++ src/cljs/megadex/app.cljs
@@ -173,6 +173,137 @@
    {:state  init-data
     :parser parser-norm}))
 
+(def dashboard-init-data
+  {:dashboard/items
+   [{:id 0 :type :dashboard/post
+     :author "Laura Smith"
+     :title "A Post!"
+     :content "Lorem ipsum dolor sit amet, quem atomorum te quo"
+     :favorites 0}
+    {:id 1 :type :dashboard/photo
+     :title "A Photo!"
+     :image "photo.jpg"
+     :caption "Lorem ipsum"
+     :favorites 0}
+    {:id 2 :type :dashboard/post
+     :author "Jim Jacobs"
+     :title "Another Post!"
+     :content "Lorem ipsum dolor sit amet, quem atomorum te quo"
+     :favorites 0}
+    {:id 3 :type :dashboard/graphic
+     :title "Charts and Stufff!"
+     :image "chart.jpg"
+     :favorites 0}
+    {:id 4 :type :dashboard/post
+     :author "May Fields"
+     :title "Yet Another Post!"
+     :content "Lorem ipsum dolor sit amet, quem atomorum te quo"
+     :favorites 0}]})
+
+(defui Post
+  static om/IQuery
+  (query [this]
+    [:title :author :content])
+  Object
+  (render [this]
+    (let [{:keys [title author content] :as props} (om/props this)]
+      (dom/div nil
+        (dom/h3 nil title)
+        (dom/h4 nil author)
+        (dom/p nil content)))))
+
+(def post (om/factory Post))
+
+(defui Photo
+  static om/IQuery
+  (query [this]
+    [:title :image :caption])
+  Object
+  (render [this]
+    (let [{:keys [title image caption]} (om/props this)]
+      (dom/div nil
+        (dom/h3 nil (str "Photo: " title))
+        (dom/div nil image)
+        (dom/p nil (str "Caption: " caption))))))
+
+(def photo (om/factory Photo))
+
+(defui Graphic
+  static om/IQuery
+  (query [this]
+    [:title :image])
+  Object
+  (render [this]
+    (let [{:keys [title image]} (om/props this)]
+      (dom/div nil
+        (dom/h3 nil (str "Graphic: " title))
+        (dom/div nil image)))))
+
+(def graphic (om/factory Graphic))
+
+(defui DashboardItem
+  static om/Ident
+  (ident [this {:keys [id type]}]
+    [type id])
+  static om/IQuery
+  (query [this]
+    (zipmap
+     [:dashboard/post :dashboard/photo :dashboard/graphic]
+     (map #(conj % :favorites :id :type)
+          [(om/get-query Post)
+           (om/get-query Photo)
+           (om/get-query Graphic)])))
+  Object
+  (render [this]
+    (let [{:keys [id type favorites] :as props} (om/props this)]
+      (dom/li
+       #js {:style #js {:padding 10 :borderBottom "1px solid black"}}
+       (dom/div nil
+         (({:dashboard/post    post
+            :dashboard/photo   photo
+            :dashboard/graphic graphic} type)
+          props))
+       (dom/div nil
+         (dom/p nil (str "Favorites: " favorites))
+         (dom/button
+          #js {:onClick
+               #(om/transact! this
+                  `[(dashboard/favorite {:ref [~type ~id]})])}
+          "Favorite!"))))))
+
+(def dashboard-item (om/factory DashboardItem))
+
+(defui Dashboard
+  static om/IQuery
+  (query [this]
+    [{:dashboard/items (om/get-query DashboardItem)}])
+  Object
+  (render [this]
+    (let [{:keys [dashboard/items]} (om/props this)]
+      (apply dom/ul
+        #js {:style #js {:padding 0}}
+        (map dashboard-item items)))))
+
+(defmulti read-dashboard om/dispatch)
+
+(defmethod read-dashboard :dashboard/items
+  [{:keys [state]} k _]
+  (let [st @state]
+    {:value (into [] (map #(get-in st %)) (get st k))}))
+
+(defmulti mutate-dashboard om/dispatch)
+
+(defmethod mutate-dashboard 'dashboard/favorite
+  [{:keys [state]} k {:keys [ref]}]
+  {:action
+   (fn []
+     (swap! state update-in (conj ref :favorites) inc))})
+
+(def dashboard-reconciler
+  (om/reconciler
+   {:state  dashboard-init-data
+    :parser (om/parser {:read read-dashboard :mutate mutate-dashboard})}))
+
 (defn init! []
   (when-let [el (gdom/getElement "container")]
     (js/ReactDOM.render
@@ -185,4 +316,6 @@
   (when-let [el (gdom/getElement "animals-container")]
     (om/add-root! animals-reconciler AnimalsList el))
   (when-let [el (gdom/getElement "norm-container")]
-    (om/add-root! norm-reconciler RootView el)))
+    (om/add-root! norm-reconciler RootView el))
+  (when-let [el (gdom/getElement "dashboard-container")]
+    (om/add-root! dashboard-reconciler Dashboard el)))
